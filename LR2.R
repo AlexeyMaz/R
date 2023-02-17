0abb18257dbca0ccf8bcc3c44c90788ea2a826c4
Al Maz <alexmaznitskiy@gmail.com>
2023-02-17 20:46:45 +0300

    4 пункт

--- LR2.R
+++ LR2.R
@@ -1,5 +1,8 @@
 df<-read.csv("Любимый жанр музыки.csv", sep=";", header=T,
                    fileEncoding="cp1251")
+
+df[15, ] <- df[16, ]
+df <- df[-c(16), ]
 View(df)
 
 num_idx <- which(sapply(df, is.numeric))
@@ -7,10 +10,41 @@ num_idx <- which(sapply(df, is.numeric))
 # №1. max, min, mean по всем столбцам
 sapply(na.omit(df[, num_idx]), max)
 sapply(na.omit(df[, num_idx]), min)
-sapply(na.omit(df[, num_idx]), mean)
+means <- sapply(na.omit(df[, num_idx]), mean)
+means
 
-# по возрастанию: 3 10 5 8 9 6 2 4 1 7
-# №3. Рейтинг
-idxes <- order(sapply(na.omit(df[, num_idx]), mean), decreasing=T)
+# №3. Рейтинг жанров по убыванию
+idxes <- order(means, decreasing=T)
 idxes <- sapply(idxes, function (x) x <- colnames(df)[x + 2])
-data.frame('Рейтинг жанров'=idxes)
+data.frame('Рейтинг_жанров'=idxes)
+
+# №4. Столбчатая диаграмма оценок
+library(ggplot2)
+
+# genre <- readline(prompt = 'Диаграмму оценок какого жанра хотите посмотреть? ')
+# # сделать первую букву заглавной
+# genre <- gsub(' ', '', 
+#               paste(toupper(substr(genre, 1, 1)), substr(genre, 2, nchar(genre))))
+
+
+# оставить только фамилию
+# surnames <- df[, 2]
+# for (i in 1 : nrow(df))
+# {
+#   first_space <- as.vector(gregexpr(' ', df[i, 2]))[[1]][1]
+#   first_space
+#   surnames[i] <- substr(df[i, 2], 1,
+#                      first_space - 1)
+# }
+
+# диаграмма
+pl_df <- data.frame(x=colnames(df[, num_idx]), y=means)
+# скип строк с NA
+pl_df <- pl_df[complete.cases(pl_df), ]
+perf <-ggplot(pl_df, aes(x, y, fill = x)) +
+  geom_bar(stat="identity", show.legend = F)
+perf
+
+# genre_str <- paste("Оценки жанра", tolower(genre))
+ggp <- perf + labs(x="Жанр",y="Оценка", title='Средняя оценка жанров музыки')
+ggp
