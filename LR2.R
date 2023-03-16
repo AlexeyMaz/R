c3fd3c26b61ec980f4fa53d9663e7e51b32f2fa6
Al Maz <alexmaznitskiy@gmail.com>
2023-03-16 17:29:08 +0300

    исправлены ошибки

--- LR2.R
+++ LR2.R
@@ -8,24 +8,17 @@ View(df)
 num_idx <- which(sapply(df, is.numeric))
 
 # №1. max, min, mean по всем столбцам
-sapply(na.omit(df[, num_idx]), max)
-sapply(na.omit(df[, num_idx]), min)
-means <- sapply(na.omit(df[, num_idx]), mean)
-means
+maxes <- sapply(na.omit(df[, num_idx]), max); maxes
+mins <- sapply(na.omit(df[, num_idx]), min); mins
+means <- sapply(na.omit(df[, num_idx]), mean); means
 
-# №2. Подсчитать количество людей, отдавших предпочтение >0.7 и <0.3
-more_than_7 <- integer(length(num_idx))
-less_than_3 <- integer(length(num_idx))
-
-more_than_7 <- sapply(df[, num_idx], function(x) length(which(x > 7)))
-less_than_3 <- sapply(df[, num_idx], function(x) length(which(x < 3)))
-more_than_7
-less_than_3
+# №2. Подсчитать количество людей, отдавших предпочтение >7 и <3
+more_than_7 <- sapply(df[, num_idx], function(x) sum(x > 7 & !is.na(x))); more_than_7
+less_than_3 <- sapply(df[, num_idx], function(x) sum(x < 3 & !is.na(x))); less_than_3
 
 # №3. Рейтинг жанров по убыванию
-idxes <- order(means, decreasing=T)
-idxes <- sapply(idxes, function (x) x <- colnames(df)[x + 2])
-data.frame('Рейтинг_жанров'=idxes)
+votes <- sort(means, decreasing = T); votes
+data.frame('Рейтинг_жанров'=votes)
 
 # №4. Столбчатая диаграмма оценок
 library(ggplot2)
@@ -47,13 +40,11 @@ library(ggplot2)
 # }
 
 # диаграмма
-pl_df <- data.frame(x=colnames(df[, num_idx]), y=means)
+pl_df <- data.frame(x=colnames(df[, num_idx]), y=means); pl_df
 # скип строк с NA
-pl_df <- pl_df[complete.cases(pl_df), ]
-perf <-ggplot(pl_df, aes(x, y, fill = x)) +
-  geom_bar(stat="identity", show.legend = F)
-perf
+# pl_df <- pl_df[complete.cases(pl_df), ]
+perf <-ggplot(pl_df, aes(x=reorder(pl_df$x, -pl_df$y), y, fill = x)) +
+  geom_bar(stat="identity", show.legend = F); perf
 
 # genre_str <- paste("Оценки жанра", tolower(genre))
-ggp <- perf + labs(x="Жанр",y="Оценка", title='Средняя оценка жанров музыки')
-ggp
+ggp <- perf + labs(x="Жанр",y="Оценка", title='Средняя оценка жанров музыки'); ggp
